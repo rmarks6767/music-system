409ea95acf5d77399d2823b4a212cec40fc43d30
rmarks6767 <rmarks6767@gmail.com>
2019-02-14 14:29:51 -0500

    Added a default path for the connection to the host

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import {exampleget} from './handlers';
 const app = Express();
 const port = 3000;
 
+app.get('/', );
 app.get('/queue', exampleget.sample);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
