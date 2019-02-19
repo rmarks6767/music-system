5de29fba2a1aba897b86ac7dbe8a4a6fcabcac0b
River Marks <rivermarks@localhost.localdomain>
2019-02-19 16:30:23 -0500

    cleaned up some of the code

--- src/index.js
+++ src/index.js
@@ -146,33 +146,6 @@ app.get('/refresh_token', function(req, res) {
 console.log('Listening on 8888');
 
 import {play} from './handlers';
-import {player} from './handlers'
-
-const Player = new Spotify.Player({
-  name: 'Car Music Player',
-  getOAuthToken: callback => {
-    // requesting access token from refresh token
-    var refresh_token = req.query.refresh_token;
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-      form: {
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        var access_token = body.access_token;
-        res.send({
-          'access_token': access_token
-        });
-      }
-    });
-  }
-});
 
 app.get('/play', play.play);
 
